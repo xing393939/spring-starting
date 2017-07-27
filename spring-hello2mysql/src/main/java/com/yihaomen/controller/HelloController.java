44d8fdff183e9bd3dfe6ff91bcec2210f5a158b6
name <email@email.com>
2017-07-27 15:14:33 +0800

    index page

--- spring-hello2mysql/src/main/java/com/yihaomen/controller/HelloController.java
+++ spring-hello2mysql/src/main/java/com/yihaomen/controller/HelloController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -14,6 +15,11 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController{
+   @RequestMapping(value = "/", method = RequestMethod.GET)
+   public String Index(Model model) {
+       return "index";
+   }
+	
    @RequestMapping(value = "/hello", method = RequestMethod.GET)
    public String printHello(ModelMap model) {
       model.addAttribute("message", "Hello Spring MVC Framework!");

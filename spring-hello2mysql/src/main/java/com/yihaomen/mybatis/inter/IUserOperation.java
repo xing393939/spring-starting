2021791b8f856ebdc7dcff86f02cd8e2573b1972
name <email@email.com>
2017-07-27 15:26:59 +0800

    hello2

--- spring-hello2mysql/src/main/java/com/yihaomen/mybatis/inter/IUserOperation.java
+++ spring-hello2mysql/src/main/java/com/yihaomen/mybatis/inter/IUserOperation.java
@@ -4,6 +4,8 @@ import com.yihaomen.mybatis.model.User;
 import com.yihaomen.mybatis.model.Article;
 
 public interface IUserOperation { 
+	
+	public User selectUserByID(int id);
 
     public List<Article> getUserArticles(int id);
 }

39cb1b0b66b8629e95786e0e6647a04cd6c90673
name <email@email.com>
2017-08-01 11:07:01 +0800

    加 slf4j 日志

--- spring-blog/src/main/java/com/yihaomen/web/index/UserController.java
+++ spring-blog/src/main/java/com/yihaomen/web/index/UserController.java
@@ -1,5 +1,6 @@
 package com.yihaomen.web.index;
 import java.util.List;
+import java.util.Arrays;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,14 +10,24 @@ import org.springframework.web.servlet.ModelAndView;
 import com.yihaomen.mybatis.inter.IUserOperation;
 import com.yihaomen.mybatis.model.Article;
 
+import org.slf4j.Logger; 
+import org.slf4j.LoggerFactory;
+
 @Controller
 public class UserController {
     @Autowired
     IUserOperation userMapper;
+    
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class); 
 
     @RequestMapping("/list")
     public ModelAndView listall(HttpServletRequest request,HttpServletResponse response){
-        List<Article> articles=userMapper.getUserArticles(1); 
+        List<Article> articles=userMapper.getUserArticles(1);
+        
+        System.out.println(articles);
+        logger.info("日志打印");
+        logger.info("now {}" , "starting server");
+        
         ModelAndView mav=new ModelAndView("list");
         mav.addObject("articles",articles);
         return mav;

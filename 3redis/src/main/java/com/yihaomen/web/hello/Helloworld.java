5d75e82097eb3726df5a921036a757f4eb9382ba
name <email@email.com>
2017-08-07 14:57:41 +0800

    三种处理json的包

--- 3redis/src/main/java/com/yihaomen/web/hello/Helloworld.java
+++ 3redis/src/main/java/com/yihaomen/web/hello/Helloworld.java
@@ -1,6 +1,8 @@
 package com.yihaomen.web.hello;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,6 +13,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.alibaba.fastjson.JSON;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
+
+
 @Controller
 public class Helloworld {
 	
@@ -20,9 +31,73 @@ public class Helloworld {
     @Autowired
     private RedisTemplate<String,User> redisTemplate;
     
+    
+    
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public void helloWorld(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.getWriter().append("hello world");
+    }
+    
+    @RequestMapping("/gson")
+    public void gson(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    	response.getWriter().println("<html>");
+
+    	ObjectMapper objectMapper = new ObjectMapper();
+        String jsonStr = "{\"data\":[{\"typeName\":\"Themes\",\"picturePath\":\"http://ndl.mgccw.com/mu/wallpaper/2014/8/18/085628192/6ae419444f544662a831b32e60032c53.png\",\"mtypeCode\":1,\"typeCode\":44,\"appNums\":2183}],\"code\":200}";
+
+        JsonParser parse =new JsonParser();  //创建JSON解析器
+        try {
+            JsonObject json=(JsonObject) parse.parse(jsonStr);  //创建jsonObject对象
+            response.getWriter().println("code:"+json.get("code").getAsInt());  //将json数据转为为int型的数据
+             
+            JsonArray array=json.get("data").getAsJsonArray();
+            for(int i = 0; i < array.size(); i ++) {
+            	JsonObject subObject = array.get(i).getAsJsonObject();
+            	
+            	response.getWriter().println("picturePath="+subObject.get("picturePath").getAsString());
+            }
+             
+        } catch (JsonIOException e) {
+            e.printStackTrace();
+        } catch (JsonSyntaxException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+    	response.getWriter().println("</html>");
+    }
+    
+    @RequestMapping("/jackson")
+    public void jackson(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    	response.getWriter().println("<html>");
+
+    	ObjectMapper objectMapper = new ObjectMapper();
+        String jsonStr = "{\"data\":[{\"typeName\":\"Themes\",\"picturePath\":\"http://ndl.mgccw.com/mu/wallpaper/2014/8/18/085628192/6ae419444f544662a831b32e60032c53.png\",\"mtypeCode\":1,\"typeCode\":44,\"appNums\":2183}],\"code\":200}";
+        Map<String, Object> data = objectMapper.readValue(jsonStr, Map.class);
+        List<Map<String, Object>> cats = (List<Map<String, Object>>) data.get("data");
+        for (Map<String, Object> cat : cats) {
+        	response.getWriter().println(cat.get("picturePath"));
+        }
+    	response.getWriter().println("</html>");
+    }
+    
+    @RequestMapping("/fastjson")
+    public void fastjson(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    	response.getWriter().println("<html>");
+
+    	User u1=new User();
+        u1.setId(1L);
+        u1.setName("wang");
+        u1.setAge(22);
+        
+        String json1 = JSON.toJSONString(u1);
+        response.getWriter().println(json1);
+        
+        User u2 = JSON.parseObject(json1, User.class);
+        response.getWriter().println(u2);
+        
+    	response.getWriter().println("</html>");
     }
     
     @RequestMapping("/redisService")

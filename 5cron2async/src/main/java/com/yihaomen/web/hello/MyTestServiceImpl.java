fb3bf97a5bc09900213de52ba0ace221d1a8d7c6
name <email@email.com>
2017-08-09 18:16:31 +0800

    sc

--- 5cron2async/src/main/java/com/yihaomen/web/hello/MyTestServiceImpl.java
+++ 5cron2async/src/main/java/com/yihaomen/web/hello/MyTestServiceImpl.java
@@ -1,7 +1,6 @@
 package com.yihaomen.web.hello;
 
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
@@ -12,11 +11,25 @@ import java.util.Date;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-@Component  //import org.springframework.stereotype.Component;  
+@Component 
 public class MyTestServiceImpl  implements IMyTestService {  
       @Scheduled(cron="0/5 * * * * ?")   //每5秒执行一次  
       @Override  
       public void myTest() {
+    	  SimpleDateFormat sdf1 = new SimpleDateFormat("yyyyMMddHHmmssSSS");
+    	  String date1= sdf1.format(new Date(System.currentTimeMillis()))+".txt";
+
+          String contents = "1111";
+          try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(
+                  new FileOutputStream(date1), "utf-8"))) {
+        	  	writer.write(contents);
+          } catch (IOException ex) {
+        	  
+          }
+          System.out.println("test ........." + date1);  
+      }
+      
+      public void myTest2() {
     	  SimpleDateFormat sdf1 = new SimpleDateFormat("yyyyMMddHHmmssSSS");
     	  String date1= sdf1.format(new Date(System.currentTimeMillis()))+".txt";
 
